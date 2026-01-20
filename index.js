eff952f792dfca936a25157aea18dc03f7d8b8fa
petr3232-cloud <petrow3232@gmail.com>
2026-01-20 12:52:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-import express from "express"
-import crypto from "crypto"
-import path from "path"
+const express = require("express");
+const crypto = require("crypto");
+const path = require("path");
 
-const app = express()
-const PORT = process.env.PORT || 8080
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 function checkTelegramAuth(initData) {
-  const urlParams = new URLSearchParams(initData)
-  const hash = urlParams.get("hash")
-  urlParams.delete("hash")
+  const urlParams = new URLSearchParams(initData);
+
+  const hash = urlParams.get("hash");
+  urlParams.delete("hash");
 
   const dataCheckString = [...urlParams.entries()]
     .sort()
     .map(([k, v]) => `${k}=${v}`)
-    .join("\n")
+    .join("\n");
 
-  const secret = crypto
+  const secretKey = crypto
     .createHmac("sha256", "WebAppData")
     .update(process.env.BOT_TOKEN)
-    .digest()
+    .digest();
 
   const hmac = crypto
-    .createHmac("sha256", secret)
+    .createHmac("sha256", secretKey)
     .update(dataCheckString)
-    .digest("hex")
+    .digest("hex");
 
-  return hmac === hash
+  return hmac === hash;
 }
 
 app.get("/", (req, res) => {
-
-  const initData = req.query.initData
+  const initData = req.query.initData;
 
   if (!initData || !checkTelegramAuth(initData)) {
-    return res.send("⛔ Доступ только через Telegram")
+    return res.send("⛔ Доступ только через Telegram");
   }
 
-  res.sendFile(path.resolve("public/index.html"))
-})
+  res.sendFile(path.resolve("public/index.html"));
+});
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT)
-})
+  console.log("Server running on port", PORT);
+});
