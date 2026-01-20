f579fbf37e7385ab045d9e4293facb4b10c37774
petr3232-cloud <petrow3232@gmail.com>
2026-01-20 13:18:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const crypto = require("crypto");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000; // fallback на локалку
 
 app.use(express.json());
 app.use(express.static("public"));
 
 function checkTelegramAuth(initData) {
+  if (!process.env.BOT_TOKEN) {
+    console.log("❌ BOT_TOKEN не найден");
+    return false;
+  }
+
   const urlParams = new URLSearchParams(initData);
+
   const hash = urlParams.get("hash");
   urlParams.delete("hash");
 
@@ -40,7 +46,11 @@ app.post("/auth", (req, res) => {
 
   const { initData } = req.body;
 
-  if (!initData || !checkTelegramAuth(initData)) {
+  if (!initData) {
+    return res.status(400).send("NO INIT DATA");
+  }
+
+  if (!checkTelegramAuth(initData)) {
     return res.status(403).send("FAKE USER");
   }
 
